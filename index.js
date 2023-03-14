cdf555dacd6ff8be055cc82b498bdc3a061d7daa
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-14 10:05:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let message;
+  if(ride <=400) {
+    message = "This one is on me!";
+  } else if (ride > 2000) {
+    message = "I will gladly take your thirty bucks.";
+  } if (ride > 2500) {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else if( city === "Pittsburgh") {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
